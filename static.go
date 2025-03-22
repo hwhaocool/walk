3c408fdc085bde28bc0051502eb3b23a2f1fae57
hwhaocool <>
2025-03-22 15:35:49 +0800

    feat: public static

--- static.go
+++ static.go
@@ -278,7 +278,7 @@ func (s *Static) WndProc(hwnd win.HWND, msg uint32, wp, lp uintptr) uintptr {
 }
 
 func staticWndProc(hwnd win.HWND, msg uint32, wp, lp uintptr) uintptr {
-	as, ok := windowFromHandle(win.GetParent(hwnd)).(interface{ asStatic() *static })
+	as, ok := windowFromHandle(win.GetParent(hwnd)).(interface{ asStatic() *Static })
 	if !ok {
 		return 0
 	}
@@ -295,7 +295,7 @@ func staticWndProc(hwnd win.HWND, msg uint32, wp, lp uintptr) uintptr {
 			Message: msg,
 			WParam:  wp,
 			LParam:  lp,
-			Pt:      win.POINT{int32(win.GET_X_LPARAM(lp)), int32(win.GET_Y_LPARAM(lp))},
+			Pt:      win.POINT{X: int32(win.GET_X_LPARAM(lp)), Y: int32(win.GET_Y_LPARAM(lp))},
 		}
 
 		return s.group.toolTip.SendMessage(win.TTM_RELAYEVENT, 0, uintptr(unsafe.Pointer(&m)))
